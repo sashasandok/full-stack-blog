ace5423557c93d3bbd6d08744c50e98580eee3d8
Alex Sandok <alex.pro.sandok@gmail.com>
2019-01-08 21:33:49 +0200

    adding routing: Post route and AddNewPost route

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Title of post</h2>
+        <p>Post Content</p>
+      </div>
+    )
+  }
+}
+
+export default Post
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,17 @@
+// react
+import React, { Component } from 'react'
+
+// components
+import Post from './Post/Post'
+
+class Posts extends Component {
+  render() {
+    return (
+      <div>
+        <Post />
+      </div>
+    )
+  }
+}
+
+export default Posts
