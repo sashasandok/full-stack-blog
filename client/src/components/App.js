984e193836a3e269bd39203061e46374b092b63a
Alex Sandok <alex.pro.sandok@gmail.com>
2019-01-08 19:00:56 +0200

    creating components adn api folders, creating api-client file and api for fetching posts

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+// react
+import React, { Component } from 'react'
+
+// styles
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">Hello Blog</header>
+        <section className="app-content">Blog Content</section>
+      </div>
+    )
+  }
+}
+
+export default App
